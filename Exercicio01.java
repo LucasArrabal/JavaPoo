5e0a0c200e393d331950f0284ebe98491b900c9d
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-10 09:22:17 -0300

    Create Exercicio01.java

---
+++ Exercicio01.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String a = new String("USCS");
+	    String b = new String("Computacao");
+	    
+	    a = b;
+	    b = a;
+	    
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
