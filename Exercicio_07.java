72214a66a798df14e7a2043743d1d0f2e9b9d2a8
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-13 08:50:09 -0300

    Create Exercicio_07.java

---
+++ Exercicio_07.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String S1 = new String("abcd");
+	    String S2 = null;
+	    S2 = S1;
+	    String S3 = new String("!!!".concat(S2.concat("!!!")));
+	    
+	    System.out.println(S1+S2+S3);
+
+	}
+}
