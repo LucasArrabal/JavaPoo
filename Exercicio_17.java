e76a68f81bce331b398c8b9d0bb2632aeb07227f
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-15 10:52:53 -0300

    Create Exercicio_17.java

---
+++ Exercicio_17.java
@@ -0,0 +1,25 @@
+public class Main
+{
+    enum TAMANHO{
+        pequeno,
+        medio,
+        grande,
+        extra_grande
+    }
+	public static void main(String[] args) {
+        TAMANHO a = TAMANHO.pequeno;
+        TAMANHO b = TAMANHO.grande;
+        boolean x = 5 != 4;
+        boolean y = true == (10<=0);
+        
+        if(x == y){
+            System.out.println(a);
+        }else{
+            if(x != y){
+                System.out.println(a);
+            }else{
+                System.out.println(b);
+            }
+        }
+	}
+}
