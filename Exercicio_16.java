7725b42afda400e19c9e9a36227aca13211746e7
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-15 10:48:31 -0300

    Create Exercicio_16.java

---
+++ Exercicio_16.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+        
+        int i = 0xFF;
+        int j = 0x0F;
+        int k = 0x00;
+        System.out.println(i & j | k);
+
+
+	}
+}
