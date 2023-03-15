5384ead425be8737128fc8609db969eaf2bba88e
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-15 10:35:06 -0300

    Create Exercicio_09.java

---
+++ Exercicio_09.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String S1 = new String("uscs");
+	    String S2 = null;
+	    S2 = "comp";
+	    String S3 = new String(S2.toUpperCase());
+
+	    System.out.println(S1+S2+S3);
+
+	}
+}
