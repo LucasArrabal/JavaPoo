92e9986e48cb3fe74bd28b2b0ca82854d4ea8a0c
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-15 10:37:32 -0300

    Create Exercicio_10.java

---
+++ Exercicio_10.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String S1 = new String("8");
+	    String S2 = null;
+	    S2 = "5" + "8";
+	    int x = Integer.parseInt(S2);
+	    int y = Integer.parseInt(S1);
+
+
+	    System.out.println(x+y);
+
+	}
+}
