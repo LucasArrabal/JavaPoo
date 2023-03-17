c834391537bb125a230e3a76a2dbba0461d133ac
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-17 08:51:26 -0300

    Create Exercicio_18.java

---
+++ Exercicio_18.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+        double nota = 9.5;
+        boolean pass = nota >= 7.0;
+        if(pass){
+            System.out.println("Aprovado...");
+        }else{
+            System.out.println("Reprovado...");
+        }
+        
+        
+	}
+}
